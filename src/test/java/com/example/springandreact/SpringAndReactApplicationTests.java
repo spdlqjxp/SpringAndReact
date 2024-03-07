b749c75e3b6a1abdd134fcb4d57be1e0a7217cfb
bagjinhwan <spdlqjxp@gmail.com>
2024-03-07 18:40:37 +0900

    Initial commit

---
+++ src/test/java/com/example/springandreact/SpringAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
